d272d153204c172b155c8bf6fd0672ad198e334f
Paul Rodriguez <andreewinquires@gmail.com>
2025-08-10 13:28:04 -0500

    Update Publisher.java

--- SportsPredictor_G6/src/main/java/observer/Publisher.java
+++ SportsPredictor_G6/src/main/java/observer/Publisher.java
@@ -7,10 +7,18 @@ public abstract class Publisher {
     protected List<Suscriptor> suscriptores = new ArrayList<>();
     protected String estadoPrincipal;
 
-    public void suscribirse(Suscriptor s) { suscriptores.add(s); System.out.println("Nuevo suscriptor agregado"); }
+    public void suscribirse(Suscriptor s) {
+        suscriptores.add(s);
+        System.out.println("Nuevo suscriptor agregado");
+    }
 
-    public void desuscribirse(Suscriptor s) { suscriptores.remove(s); System.out.println("Suscriptor removido"); }
+    public void desuscribirse(Suscriptor s) {
+        suscriptores.remove(s);
+        System.out.println("Suscriptor removido");
+    }
 
-    public void notificarSuscriptor() { for (Suscriptor s : suscriptores) s.update(estadoPrincipal); }
+    public void notificarSuscriptor() {
+        for (Suscriptor s : suscriptores) s.update(estadoPrincipal);
+    }
 
 }
