404399954a123c89aa486fc92b42d09674aadd2d
Andie Barreno <151972823+hetthie@users.noreply.github.com>
2025-08-10 14:42:26 -0500

    Update ParticipanteBasket.java

--- SportsPredictor_G6/src/main/java/model/participante/ParticipanteBasket.java
+++ SportsPredictor_G6/src/main/java/model/participante/ParticipanteBasket.java
@@ -2,23 +2,23 @@ package model.participante;
 
 public class ParticipanteBasket extends Participante {
     private Integer puntos;
-    private Integer rebotes;
+    private Integer canastas;
 
     public ParticipanteBasket(String nombre, String posicion) {
         super(nombre);
         this.puntos = 0;
-        this.rebotes = 0;
+        this.canastas = 0;
     }
 
     @Override
     public void actualizarEstadisticas() {
         estadisticas.add(puntos);
-        estadisticas.add(rebotes);
+        estadisticas.add(canastas);
     }
 
     public void registrarPunto() {
-        this.puntos += 2;
-        System.out.println(nombre + " ha anotado 2 puntos!");
+        this.puntos += 1;
+        System.out.println(" ha anotado otro puntos!");
     }
 
 }
