23338b966dfe1c6d6c7cec6c8c1c1d0e4b418b8c
Paul Rodriguez <andreewinquires@gmail.com>
2025-08-10 15:59:23 -0500

    Update EstrategiaPuntaje.java

--- SportsPredictor_G6/src/main/java/strategy/EstrategiaPuntaje.java
+++ SportsPredictor_G6/src/main/java/strategy/EstrategiaPuntaje.java
@@ -2,4 +2,6 @@ package strategy;
 
 import model.prediccion.Prediccion;
 
-public interface EstrategiaPuntaje { int calcularPuntaje(Prediccion prediccion); }
+public interface EstrategiaPuntaje {
+  int calcularPuntaje(Prediccion prediccion);
+}
