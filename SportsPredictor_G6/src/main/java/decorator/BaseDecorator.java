caa9357f7df426efce00d01c53e65d529aa3674b
Andie Barreno <151972823+hetthie@users.noreply.github.com>
2025-08-10 14:09:57 -0500

    Update BaseDecorator.java

--- SportsPredictor_G6/src/main/java/decorator/BaseDecorator.java
+++ SportsPredictor_G6/src/main/java/decorator/BaseDecorator.java
@@ -3,10 +3,16 @@ package decorator;
 public abstract class BaseDecorator implements Notificador {
     protected Notificador envolviendo;
 
-    public BaseDecorator(Notificador n){ this.envolviendo = n; }
+    public BaseDecorator(Notificador n){ 
+        this.envolviendo = n; 
+    }
 
-    @Override public void update(String contexto){ if(envolviendo!=null) envolviendo.update(contexto); }
+    @Override public void update(String contexto){ 
+        if(envolviendo!=null) envolviendo.update(contexto); 
+    }
 
-    @Override public void enviar(String mensaje){ if(envolviendo!=null) envolviendo.enviar(mensaje); }
+    @Override public void enviar(String mensaje){ 
+        if(envolviendo!=null) envolviendo.enviar(mensaje); 
+    }
 
 }
