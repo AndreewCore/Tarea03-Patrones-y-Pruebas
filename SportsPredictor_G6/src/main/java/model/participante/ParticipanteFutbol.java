a101dadb8748b1865a841107129c1d3f2663c8c9
FrancisJ04 <frajvelo@espol.edu.ec>
2025-08-10 20:44:42 -0500

    Add creational factory tests

--- SportsPredictor_G6/src/main/java/model/participante/ParticipanteFutbol.java
+++ SportsPredictor_G6/src/main/java/model/participante/ParticipanteFutbol.java
@@ -23,4 +23,8 @@ public class ParticipanteFutbol extends Participante {
         System.out.println(nombre + " ha marcado un gol!");
     }
 
+    public String getPosicion() {
+        return posicion;
+    }
+
 }
